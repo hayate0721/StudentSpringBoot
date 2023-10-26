8c080661bc70322c2c4b705eb5299a883660dc66
hayate0721 <h08066802495@gmail.com>
2023-10-25 23:08:10 -0700

    Initial commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(
+                        1L,
+                        "Hayate",
+                        "hayate@gamail.com",
+                        LocalDate.of(2001, Month.JULY, 21),
+                        22
+                )
+        );
+
+    }
+
+}
